f451b714fb0dc0fc272e8729597b6f76c50e2de4
Albin Severinson <albin@severinson.org>
2020-09-01 13:02:05 +0200

    Initial commit

--- src/GradientSketching.jl
+++ src/GradientSketching.jl
@@ -1,5 +1,142 @@
 module GradientSketching
 
-# Write your package code here.
+using LinearAlgebra, Random
+
+export BiasSEGA, SEGA
+export project!, projecta!, gradient, gradient!
+
+"""
+
+Biased SEGA gradient estimator
+
+"""
+struct BiasSEGA{T,N}
+    h::Array{T,N}
+end
+
+BiasSEGA{T}(dims::Tuple) where T = BiasSEGA{T,length(dims)}(zeros(T, dims))
+BiasSEGA(dims::Tuple) = BiasSEGA{Float64}(dims)
+BiasSEGA{T}(dim::Integer) where T = BiasSEGA{T}((dim,))
+BiasSEGA(dim::Integer) = BiasSEGA{Float64}((dim,))
+
+Base.eltype(sega::BiasSEGA{T}) where T = T
+Base.size(sega::BiasSEGA) = size(sega.h)
+Base.size(sega::BiasSEGA, i::Integer) = size(sega.h, i)
+Base.show(io::IO, sega::BiasSEGA) = print(io, "BiasSEGA{$(eltype(sega)),$(size(sega))")
+
+"""
+    project!(sega::BiasSEGA{T,1}, s∇::Number, s::AbstractVector; Binv=I) where T
+
+Update the gradient estimate `h` in-place with the one-dimensional gradient sketch `s'*∇`, where
+`s` is the sketching vector and `∇` is the gradient. The updated estimate is the projection of
+`h` onto the set of arrays `y` satisfying `s'*y = s'*∇`.
+"""
+function project!(sega::BiasSEGA{T,1}, s∇::Number, s::AbstractVector; Binv=I) where T
+    length(s) == size(sega, 1) || throw(DimensionMismatch("s dimensions ($(length(s)),1), h has dimensions $(size(sega))"))
+    Binv == I || size(Binv) == (length(s), length(s)) || throw(DimensionMismatch("Binv has dimensions $(size(Binv)), s has dimensions ($(length(s)),1)"))    
+    P = s ./ dot(s, Binv, s)
+    sega.h .-= Binv * P * (dot(s, sega.h) - s∇)
+end
+
+"""
+    project!(sega::BiasSEGA, S∇, S::AbstractMatrix; Binv=I)
+
+Update the gradient estimate `h` in-place with the gradient sketch `S'*∇`, where
+`S` is the sketching matrix and `∇` is the gradient. The updated estimate is the projection of
+`h` onto the set of arrays `y` satisfying `S'*y = S'*∇`.
+"""
+function project!(sega::BiasSEGA, S∇, S::AbstractMatrix; Binv=I)
+    size(S, 1) == size(sega, 1) || throw(DimensionMismatch("S has dimensions $(size(S)), h has dimensions $(size(sega))"))
+    size(S∇)[2:end] == size(sega)[2:end] || throw(DimensionMismatch("S∇ has dimensions $(size(S∇)), h has dimensions $(size(sega))"))
+    Binv == I || size(Binv) == (length(s), length(s)) || throw(DimensionMismatch("Binv has dimensions $(size(Binv)), S has dimensions $(size(S))"))
+    StS = S'*Binv*S
+    sega.h .-= Binv*S*(StS \ (S'*sega.h .- S∇))
+end
+
+project!(sega::BiasSEGA, S∇, s::AbstractVector; Binv=I) = project!(sega, S∇, reshape(s, length(s), 1), Binv=Binv)
+
+"""
+    projecta!(sega::BiasSEGA, S∇, S::AbstractMatrix; Binv=I, γ::Integer=5)
+
+Approximate version of `project!` that projects onto each column of `S` separately. Iterates over a random permutation
+of the columns `γ` times. This method is equivalent to `project!` if the columns of `S` are orthogonal.
+"""
+function projecta!(sega::BiasSEGA, S∇, S::AbstractMatrix; Binv=I, γ::Integer=5)
+    γ >= 1 || throw(DomainError("γ must be positive."))
+    p = collect(1:size(S, 1))
+    for _ in 1:γ
+        shuffle!(p)
+        for i in p
+            project!(sega, selectdim(S∇, 1, i:i), view(S, :, i), Binv=Binv)
+        end
+    end
+    sega.h
+end
+
+"""
+    projecta!(sega::BiasSEGA, S∇::AbstractVector, S::AbstractMatrix; Binv=I, γ::Integer=5)
+
+Approximate version of `project!` for vector gradients.
+"""
+function projecta!(sega::BiasSEGA, S∇::AbstractVector, S::AbstractMatrix; Binv=I, γ::Integer=5)
+    γ >= 1 || throw(DomainError("γ must be positive."))
+    p = collect(1:size(S, 1))
+    for _ in 1:γ
+        shuffle!(p)
+        for i in p
+            project!(sega, S∇[i], view(S, :, i), Binv=Binv)
+        end
+    end
+    sega.h
+end
+
+projecta!(sega::BiasSEGA, S∇, s::AbstractVector; Binv=I, γ::Integer=5) = project!(sega::BiasSEGA, S∇, s::AbstractVector; Binv=Binv)
+
+gradient!(∇, sega::BiasSEGA) = ∇ .= sega.h
+gradient(sega::BiasSEGA) = gradient!(zero(sega.h), sega)
+
+"""
+
+SEGA gradient estimator
+
+"""
+struct SEGA{T,N}
+    θ::T                # Bias removal coefficient
+    hp::Array{T,N}      # Previous estimate
+    h::BiasSEGA{T,N}    # Biased gradient estimate
+    g::Array{T,N}       # Unbiased gradient estimate
+end
+
+SEGA{T}(θ, dims::Tuple) where T = SEGA{T,length(dims)}(θ, zeros(T, dims), BiasSEGA{T}(dims), zeros(T, dims))
+SEGA(θ, dims::Tuple) = SEGA{Float64}(θ, dims)
+SEGA{T}(θ, dim::Integer) where T = SEGA{T}(θ, (dim,))
+SEGA(θ, dim::Integer) = SEGA{Float64}(θ, (dim,))
+
+Base.eltype(sega::SEGA{T}) where T = T
+Base.size(sega::SEGA) = size(sega.h)
+Base.size(sega::SEGA, i::Integer) = size(sega.h, i)
+Base.show(io::IO, sega::SEGA) = print(io, "SEGA{$(eltype(sega)),$(size(sega))")
+
+function project!(sega::SEGA, args...; kwargs...)
+    gradient!(sega.hp, sega.h)
+    project!(sega.h, args...; kwargs...)    
+end
+
+function projecta!(sega::SEGA, args...; kwargs...)
+    gradient!(sega.hp, sega.h)
+    projecta!(sega.h, args...; kwargs...)
+end
+
+"""
+
+Compute an unbiased estimate of the gradient.
+
+"""
+function gradient!(∇::Array{T,N}, sega::SEGA{T,N}) where {T,N}
+    gradient!(∇, sega.h) # store the current (biased) estimate in ∇
+    ∇ .*= sega.θ
+    ∇ .+= (1-sega.θ) .* sega.hp
+end
+gradient(sega::SEGA) = gradient!(zero(sega.hp), sega)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,167 @@
 using GradientSketching
 using Test
 
-@testset "GradientSketching.jl" begin
-    # Write your tests here.
+@testset "BiasSEGA" begin
+
+    ### Constructors
+    sega = BiasSEGA((10, 5))
+    @test eltype(sega) == Float64
+    @test size(sega) == (10, 5)    
+
+    sega = BiasSEGA{Float32}((10, 5))
+    @test eltype(sega) == Float32
+    @test size(sega) == (10, 5)        
+
+    sega = BiasSEGA(3)
+    @test eltype(sega) == Float64
+    @test size(sega) == (3, )          
+
+    sega = BiasSEGA{Float32}(3)
+    @test eltype(sega) == Float32
+    @test size(sega) == (3, )
+
+    ### Vector sketches
+    sega = BiasSEGA(2)
+    ∇ = zeros(2)
+
+    # Project onto the first dimension
+    project!(sega, 1, [1, 0])
+    @test gradient(sega) ≈ [1, 0]
+    gradient!(∇, sega)
+    @test ∇ ≈ [1, 0]    
+
+    # Project onto the second dimension
+    project!(sega, 1, [0, 1])
+    @test gradient(sega) ≈ [1, 1]
+    gradient!(∇, sega)
+    @test ∇ ≈ [1, 1]
+
+    ### 1-dimensional matrix sketches for 2-dimensional gradients
+    sega = BiasSEGA((2, 3))
+    ∇ = zeros(2, 3)
+    correct = zeros(2, 3)
+
+    # Project onto the first dimension
+    correct[1, :] .= 1
+    project!(sega, ones(1, 3), [1 0]')
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct
+
+    # Project onto the second dimension
+    correct[2, :] .= 1
+    project!(sega, ones(1, 3), [0 1]')
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct
+
+    # Vector sketches for 2-dimensional gradients
+    sega = BiasSEGA((2, 3))
+    ∇ = zeros(2, 3)
+    correct = zeros(2, 3)
+
+    # Project onto the first dimension
+    correct[1, :] .= 1
+    project!(sega, ones(1, 3), [1, 0])
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct
+
+    # Project onto the second dimension
+    correct[2, :] .= 1
+    project!(sega, ones(1, 3), [0, 1])
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct    
+
+    ### Matrix sketches
+    sega = BiasSEGA((2, 3))
+    ∇ = zeros(2, 3)
+    correct = ones(2, 3)
+    S = [1 2; 3 4]
+    project!(sega, S'*correct, S)    
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct
+
+    ### Vector sketches with approximate projection
+    sega = BiasSEGA((2, 3))
+    ∇ = zeros(2, 3)
+    correct = zeros(2, 3)
+    
+    # Project onto the first dimension
+    correct[1, :] .= 1
+    projecta!(sega, ones(1, 3), [1, 0])
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct
+
+    # Project onto the second dimension
+    correct[2, :] .= 1
+    projecta!(sega, ones(1, 3), [0, 1])
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct        
+
+    ### Matrix sketches for 1-dimensional gradient with approximate projection
+    # Test that we get arbitrarily close after a very large number of iterations
+    sega = BiasSEGA(2)
+    ∇ = zeros(2)
+    correct = ones(2)
+    S = [1 2; 3 4]
+    projecta!(sega, S'*correct, S, γ=10000)    
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct    
+
+    ### Matrix sketches for 2-dimensional gradient with approximate projection
+    # Test that we get arbitrarily close after a very large number of iterations
+    sega = BiasSEGA((2, 3))
+    ∇ = zeros(2, 3)
+    correct = ones(2, 3)
+    S = [1 2; 3 4]
+    projecta!(sega, S'*correct, S, γ=10000)    
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct
+end
+
+@testset "SEGA" begin
+
+    ### Constructors
+    sega = SEGA(1, (10, 5))
+    @test eltype(sega) == Float64
+    @test size(sega) == (10, 5)    
+
+    sega = SEGA{Float32}(1, (10, 5))
+    @test eltype(sega) == Float32
+    @test size(sega) == (10, 5)        
+
+    sega = SEGA(1, 3)
+    @test eltype(sega) == Float64
+    @test size(sega) == (3, )          
+
+    sega = SEGA{Float32}(1, 3)
+    @test eltype(sega) == Float32
+    @test size(sega) == (3, )      
+
+    ### Matrix sketches
+    sega = SEGA(1, (2, 3))
+    ∇ = zeros(2, 3)
+    correct = ones(2, 3)
+    S = [1 2; 3 4]
+    project!(sega, S'*correct, S)
+    @test gradient(sega) ≈ correct
+    gradient!(∇, sega)
+    @test ∇ ≈ correct
+
+    ### Matrix sketches with θ != 1
+    sega = SEGA(1/2, (2, 3))
+    ∇ = zeros(2, 3)
+    correct = ones(2, 3)
+    S = [1 2; 3 4]
+    project!(sega, S'*correct, S)
+    @test gradient(sega) ≈ correct ./ 2
+    gradient!(∇, sega)
+    @test ∇ ≈ correct ./ 2
 end
